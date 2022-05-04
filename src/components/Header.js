268a7ace5f6de90ebaf1fa4701cce4bf970cb10e
Juan Valencia <juan.vo.11.2000@gmail.com>
2022-05-04 17:05:31 -0500

    version tres
    version tres

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,19 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import svg from '../img/pexels-seeklogo.com.svg';
+import svg from '../img/pexels-seeklogo.png';
 
 export const Header = () => {
-
-  const [toggle, setToggle] = useState(false);
-
-  const handleToggle = () =>{ 
-    toggle == true ? setToggle(false) : setToggle(true) ;
-  }
 
 
   return (
     <div className='flex justify-between bg-gray-800  w-full z-50 items-center'>
       <Link to="/" >
-        <img src={'../img/pexels-seeklogo.com.svg'} alt="" className='w-12 m-4'/>
+        <img src={svg} alt="pexels" className='w-12 m-4'/>
       </Link>
       <Link to="/">
         <h3 className='text-2xl text-white m-4'>Clon Pexels</h3>
       </Link>
-     {/* <i className='bx bx-menu text-5xl m-4 hover:cursor-pointer text-white' id="toggle" onClick={handleToggle}></i>
-    <ul className= {toggle === true ? 'absolute  flex-col w-full min-h-full mt-16  text-4xl text-center  bg-gray-800 text-white ease-in duration-500 ' : 'hidden'} >
-      <li className='leading-loose'>Opción 1</li>
-      <li className='leading-loose'>Opción 2</li>
-      <li className='leading-loose'>Opción 3</li>
-    </ul> */}
+  
     </div>
   )
 }
