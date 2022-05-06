f0cecdce9873394162811bb021c3f3f0eb19b2ac
Juan Valencia <juan.vo.11.2000@gmail.com>
2022-05-06 10:56:24 -0500

    Docs actualizado

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ClonPexel } from './ClonPexel';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='clon-pexels'>
+    <BrowserRouter >
     <ClonPexel />
     </BrowserRouter>
   </React.StrictMode>
